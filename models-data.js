a4ae8d22d0fa7379e5d344d2592365184545337a
TehH3ro0fTiem <ryan.m.streeter@gmail.com>
2026-09-10 12:18:23 -0500

    Create models-data.js

---
+++ models-data.js
@@ -0,0 +1,39 @@
+const MODELS = [
+  {
+    id: "leon-kennedy",
+    title: "Leon Kennedy - Resident Evil 9 Requiem",
+    images: [
+      "images/leon-kennedy.jpg",
+      "images/leon-kennedy-2.jpg",
+      "images/leon-kennedy-3.jpg"
+    ],
+    scale: "1/6 Scale",
+    size: "Approx. 11.5 in (29 cm) tall",
+    description: "Original resin sculpt inspired by Leon Kennedy's appearance in Resident Evil 9: Requiem. Hand-finished and primed, ready for painting or display as-is.",
+    etsyUrl: "https://www.etsy.com/listing/4495283661/leon-kennedy-resident-evil-9-requiem"
+  },
+  {
+    id: "spiderman-venom",
+    title: "Spider-Man vs Venom",
+    images: [
+      "images/spiderman-venom.jpg",
+      "images/spiderman-venom-2.jpg"
+    ],
+    scale: "1/8 Scale",
+    size: "Approx. 9 in (23 cm) tall",
+    description: "A dynamic diorama capturing Spider-Man and Venom mid-clash, sculpted with dramatic action posing and slime detailing.",
+    etsyUrl: "https://www.etsy.com/listing/4495843541/spider-man-vs-venom-resin-model-kit-3d"
+  },
+  {
+    id: "hunter-bloodborne",
+    title: "The Hunter - Bloodborne",
+    images: [
+      "images/hunter-bloodborne.jpg",
+      "images/hunter-bloodborne-2.jpg"
+    ],
+    scale: "1/6 Scale",
+    size: "Approx. 12 in (30 cm) tall",
+    description: "A moody, atmospheric sculpt of the Hunter from Bloodborne, cloaked and ready for the hunt.",
+    etsyUrl: "https://www.etsy.com/listing/4495305898/the-hunter-bloodborne-resin-model-kit-3d"
+  }
+];
