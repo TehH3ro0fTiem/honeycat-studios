db90810b3054ad5f75bf04d3259975922de6d8b8
TehH3ro0fTiem <ryan.m.streeter@gmail.com>
2026-09-10 14:37:45 -0500

    Update models-data.js

--- models-data.js
+++ models-data.js
@@ -27,8 +27,8 @@ const MODELS = [
     series: "Marvel",
     title: "Spider-Man vs Venom",
     images: [
-      "images/spiderman-venom.jpg",
-      "images/spiderman-venom-2.jpg"
+      "images/tanuki-figures/marvel/spider-man/spiderman-venom-1.jpg",
+      "images/tanuki-figures/marvel/spider-man/spiderman-venom-2.jpg"
     ],
     scale: "1/8 Scale",
     size: "Approx. 9 in (23 cm) tall",
@@ -41,12 +41,61 @@ const MODELS = [
     series: "Bloodborne",
     title: "The Hunter - Bloodborne",
     images: [
-      "images/hunter-bloodborne.jpg",
-      "images/hunter-bloodborne-2.jpg"
+      "images/kc-studio/bloodborne/hunter/hunter-bloodborne-1.jpg",
+      "images/kc-studio/bloodborne/hunter/hunter-bloodborne-2.jpg"
     ],
     scale: "1/6 Scale",
     size: "Approx. 12 in (30 cm) tall",
     description: "A moody, atmospheric sculpt of the Hunter from Bloodborne, cloaked and ready for the hunt.",
     etsyUrl: "https://www.etsy.com/listing/4495305898/the-hunter-bloodborne-resin-model-kit-3d"
+  }
+  {
+    id: "bulkamancer-malenia",
+    artist: "Bulkamancer",
+    series: "Elden Ring",
+    title: "Malenia, Blade of Miquella - Goddess of Rot",
+    images: [
+      "images/bulkamancer/elden-ring/malenia/malenia-1.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-2.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-3.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-4.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-5.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-6.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-7.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-8.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-9.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-10.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-11.jpg",
+      "images/bulkamancer/elden-ring/malenia/malenia-12.jpg"
+    ],
+    scale: "1/6 Scale",
+    size: "Approx. 12 in (30 cm) tall",
+    description: "A moody, atmospheric sculpt of the Hunter from Bloodborne, cloaked and ready for the hunt.",
+    etsyUrl: "https://www.etsy.com/listing/4510471991/scarlet-lady-blade-of-rot-ring-of-elden"
+  }
+  {
+    id: "ca3d-spidey",
+    artist: "CA3D Studios",
+    series: "Marvel",
+    title: "Spider-Man on Goblin Glider",
+    images: [
+      "images/ca3d/marvel/spider-man/spidey-glider-1.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-2.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-3.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-4.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-5.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-6.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-7.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-8.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-9.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-10.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-11.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-12.jpg",
+      "images/ca3d/marvel/spider-man/spidey-glider-13.jpg"
+    ],
+    scale: "1/6 Scale",
+    size: "Approx. 12 in (30 cm) tall",
+    description: "A moody, atmospheric sculpt of the Hunter from Bloodborne, cloaked and ready for the hunt.",
+    etsyUrl: "https://www.etsy.com/listing/4497859644/spider-man-3d-printed-scale-resin-statue"
   }
 ];
